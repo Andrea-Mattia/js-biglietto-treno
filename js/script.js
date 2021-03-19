269babde0333d87977afbe1e26c841079d00de04
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-03-19 18:03:27 +0100

    add htm + css & base func javascript

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('here i am');
+/**
+ * GENERATING TICKET PRICE BY KM AND AGE
+ */
+
+function myFunction() {
+    //GET KM DATA
+    var kilo = document.getElementById('kilometers').value;
+    //GET AGE DATA
+    var age = document.getElementById('age').value;
+    //GENERATE PRICE
+    var price = kilo * 0.27;
+    //INSERT PRICE IN HTML
+    document.getElementById('ticket-price').innerHTML = price + '€';
+}
