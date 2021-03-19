1765ca7b89ed99cd9b21a3d5e042aee63f9397d1
Andrea-Mattia <andrea.mattia94@gmail.com>
2021-03-19 19:04:08 +0100

    add if section to generate discount

--- js/script.js
+++ js/script.js
@@ -4,11 +4,39 @@
 
 function myFunction() {
     //GET KM DATA
-    var kilo = document.getElementById('kilometers').value;
+    var kilo = parseInt(document.getElementById('kilometers').value);
+
     //GET AGE DATA
-    var age = document.getElementById('age').value;
+    var age = parseInt(document.getElementById('age').value);
+
+    //VALIDATOR
+    if ( (isNaN(kilo)) || (isNaN(age))) {
+        alert('Attention! Insert a number!');
+        return;
+    }
+
     //GENERATE PRICE
+    //IF SECTION
     var price = kilo * 0.27;
+    var discPerc;
+    var discPrice;
+
+    // Calculating discount if <18 or >=65
+    if (age < 18) {
+        discPerc = (price / 100) * 20;
+        discPrice = price - discPerc;
+        price = discPrice;
+        document.getElementById('discount').innerHTML = 'You have a price reduction!';
+    } else if (age >= 65) {
+        discPerc = (price / 100) * 40;
+        discPrice = price - discPerc;
+        price = discPrice;
+        document.getElementById('discount').innerHTML = 'You have a price reduction!';
+    } else {
+        //added to remove 'you have a price red.'
+        document.getElementById('discount').innerHTML = '';
+    }
+    
     //INSERT PRICE IN HTML
-    document.getElementById('ticket-price').innerHTML = price + '€';
+    document.getElementById('ticket-price').innerHTML = price.toFixed(2) + '€';
 }
